1ff2ad5aa27c316f145259404384cf9906f4078e
DJ Adams <dj.adams@pobox.com>
2014-02-28 17:50:27 +0000

    No searchfield, added footer bar, tidied layout

--- view/Carriers.controller.js
+++ view/Carriers.controller.js
@@ -4,9 +4,18 @@ sap.ui.controller("com.bf.newtrial.view.Carriers", {
         this.navigation.navTo("Flights", oEvent.getSource().getBindingContext());
     },
 
-    handleListSelect : function (oEvent) {
+    handleListSelect: function (oEvent) {
         this.navigation.navTo("Flights", oEvent.getParameter("listItem").getBindingContext());
     },
 
+    handleSearch: function(oEvent) {
+        var oBinding = this.getView().byId("list").getBinding("items");
+        var aFilters = [];
+        var sQuery = oEvent.getParameter("query");
+        if (sQuery && sQuery.length) {
+            aFilters.push(new sap.ui.model.Filter("Carrname", sap.ui.model.FilterOperator.Contains, sQuery));
+        }
+        oBinding.filter(aFilters);
+    }
 
 });
